4bc28db4c0eb57fb4885db99c4bbb0127afeccc3
Marcelo Lazaroni <lazaronijunior@gmail.com>
2020-04-04 00:15:47 +0100

    Use test commands

--- src/Strategies.hs
+++ src/Strategies.hs
@@ -9,6 +9,7 @@ import Data.List (mapAccumL)
 import Data.Monoid ((<>))
 import Data.Foldable (fold)
 import Data.Functor.Identity (Identity, runIdentity)
+import Control.Monad (void)
 import Control.Monad.State (State)
 import Data.Tree (Tree(..), drawTree)
 
@@ -43,35 +44,53 @@ data Info = Info
     }
     deriving (Show, Eq)
 
-newtype Arity = Arity Int
-    deriving (Show, Eq)
-
 class Monad s => Strategy s where
     -- | By being polymorphic on the response condition, we prevent
     -- this function from accidentally misdiagnosing the input.
-    evaluateNode :: Info -> Bool -> condition -> s (Maybe condition)
-    -- | Mix the samples of everyone in this tree and return a result
-    -- If anyone is infected, the result will be "Infected"
-    test :: LeafTree Condition -> s Condition
-    test tree = return $ fold tree
+    evaluateNode :: Info -> Bool -> s (TestCmd s)
+
+data TestCmd m
+    -- | This callback allows us to process the output of the test
+    = RunTest (Condition -> m ())
+    | SkipTest
+
+-- | Make sure that if the test is run, the callback
+-- in RunTest will be called.
+runTestCmd :: Monad m => LeafTree Condition -> TestCmd m -> m (Maybe Condition)
+runTestCmd tree cmd =
+    case cmd of
+        SkipTest  -> return Nothing
+        RunTest f -> do
+            let result = test tree
+            f result
+            return $ Just result
+
+-- | Mix the samples of everyone in this tree and return a result
+-- If anyone is infected, the result will be "Infected"
+test :: LeafTree Condition -> Condition
+test = fold
 
 -- | Evaluate a tree according to a strategy
 evaluateTree :: Strategy s => Info -> LeafTree Condition -> s ResultTree
 evaluateTree info tree =
     case tree of
         Leaf condition -> do
-            result <- evaluateNode info True condition
+            result <- runTestCmd tree =<< evaluateNode info True
             return $ Node result []
 
         LNode subtrees -> do
-            nodeResult    <- evaluateNode info False =<< test tree
-            childrenNodes <- traverse (evaluateTree info) subtrees
-            return $ Node nodeResult childrenNodes
+            cmd        <- evaluateNode info False
+            nodeResult <- runTestCmd tree cmd
+            Node nodeResult <$> traverse (evaluateTree info) subtrees
 
 -- | Check whether the result provided is correct
 isValid :: LeafTree Condition -> ResultTree -> Bool
 isValid = undefined
 
+
+-- | Do nothing
+noop :: Monad m => a -> m ()
+noop _ = return ()
 
 -------------------------------------------------------------------------------
 -- Strategy to test all leaves and test no intermediary nodes.
@@ -84,11 +103,10 @@ runTestLeaves :: TestLeaves a -> a
 runTestLeaves (TestLeaves i) = runIdentity i
 
 instance Strategy TestLeaves where
-    evaluateNode info isLeave condition =
-        if isLeave
-           then return (Just condition)
-           else return Nothing
-    test tree = return $ fold tree
+    evaluateNode info isLeave =
+        return $ if isLeave
+           then RunTest noop
+           else SkipTest
 
 -------------------------------------------------------------------------------
 -- Strategy to test only nodes at odd level numbers
@@ -100,14 +118,12 @@ runOddStrategy :: OddStrategy a -> a
 runOddStrategy (OddStrategy s) = State.evalState s 0
 
 instance Strategy OddStrategy where
-    evaluateNode info isLeave condition = do
+    evaluateNode info isLeave = do
         index <- State.get
         State.put $ index + 1
-        if odd index
-           then return (Just condition)
-           else return Nothing
-    test tree = return $ fold tree
-
+        return $ if odd index
+           then RunTest noop
+           else SkipTest
 
 -------------------------------------------------------------------------------
 
